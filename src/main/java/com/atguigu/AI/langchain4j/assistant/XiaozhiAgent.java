f6b9dee2cfab0823998c91d2fc163ced4d2c9ad0
XFFZKA <3201455355@qq.com>
2025-06-05 22:51:15 +0800

    function calling

--- src/main/java/com/atguigu/AI/langchain4j/assistant/XiaozhiAgent.java
+++ src/main/java/com/atguigu/AI/langchain4j/assistant/XiaozhiAgent.java
@@ -13,7 +13,7 @@ import dev.langchain4j.service.spring.AiServiceWiringMode;
         chatMemoryProvider = "chatMemoryProviderXiaozhi",
         tools = "appointmentTools" //tools配置
 )
-
+//
 public interface XiaozhiAgent {
 
     @SystemMessage(fromResource = "zhaozhi-prompt-template.txt")
